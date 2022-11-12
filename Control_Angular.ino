26a94e60ef286eb320d4c144c3ef700087b79ed6
marc0cl <63360454+marc0cl@users.noreply.github.com>
2022-11-12 02:33:14 -0300

    Fixed POS

--- Control_Angular.ino
+++ Control_Angular.ino
@@ -3,20 +3,17 @@ int setpoint = 0;
 int last_error = 0;
 int pot_limite = 250;
 
-int PIDLambo(int POS, float Kp, float Kd, float Ki) {
+int PIDLambo(int POS, float Kp, float Kd, float Ki)
+{
 
-  int error = pos - setpoint;
+  int error = POS - setpoint;
   int derivative = error - last_error;
   last_error = error;
   int pot_giro = (error * Kp + derivative * Kd);
-
-
 
   if (pot_giro > pot_limite)
     pot_giro = pot_limite;
   else if (pot_giro < -pot_limite)
     pot_giro = -pot_limite;
   return pot_giro;
-
-
 }
