c789cfae63c8bfea34b99863df0aa35682e27685
eziron <kraedon.rf@gmail.com>
2022-11-12 04:12:08 -0300

    triggers

--- Hits.ino
+++ Hits.ino
@@ -15,18 +15,57 @@ void readSideSensors() {
 
 void getGeo() {
 
-  if ((HL == 0) && (HR == 0)) {
-    geo = 0;
-  }
-
-  if(!HL){
-    if(trigger[0]){
-      trigger[0] = 0;
-      HL_T = 1;
+  //trigger especificao HL
+  if(HL){
+    if(!trigger[0]){
+      trigger[0] = 1;
      }
   }
   else{
-    trigger[0] = 1;
+    if(trigger[0]){
+      trigger[0] = 0;
+      if(!trigger[2]){
+        //codigo funcional del trigger
+        curve_sensor();
+      }
+     }
   }
 
+  if(HR){
+    if(!trigger[1]){
+      trigger[1] = 1;
+     }
+  }
+  else{
+    if(trigger[1]){
+      trigger[1] = 0;
+      if(!trigger[2]){
+        //codigo funcional del trigger
+        finish_sensor();
+      }
+     }
+  }
+
+  //trigger general
+  if(HL && HR){
+    if(!trigger[2]){
+      trigger[2] = 1;
+     }
+  }
+  else if (!HL && !HR)
+  {
+    trigger[2] = 0;
+  }
+}
+
+void curve_sensor(){
+  tone(PINBUZZER, 2500, 50);
+  //esta funcion se ejecuta cada ves que se pase por las lineas 
+  //que indican el cambio de una curva/recta
+}
+
+void finish_sensor(){
+  tone(PINBUZZER, 3000, 50);
+  //esta funcion se ejecuta cada ves que se pase por las lineas 
+  //que indican el inicio o termino de la pista
 }
