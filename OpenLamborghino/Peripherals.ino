4469c335d6452e560ff0f060fe5168a112a958a9
Andrés Bercovich <abercovich@lamborghino.com>
2023-07-07 12:58:07 -0400

    #Ok

--- OpenLamborghino/Peripherals.ino
+++ OpenLamborghino/Peripherals.ino
@@ -21,29 +21,3 @@ void Peripherals_init() {
   pinMode(PINBOTON, INPUT); // El pin del botón se configura como entrada
   pinMode(PINBUZZER, OUTPUT); // El pin del buzzer se configura como salida
   pinMode(PINLED, OUTPUT); // El pin del LED se configura como salida
-// Definición de pines
-#define PINBUZZER 10
-#define PINBOTON 2
-#define PINLED 13
-
-// Función para esperar el botón
-void WaitBoton() {
-  // Entra en un bucle infinito de espera hasta que se presione el botón
-  while (!digitalRead(PINBOTON)); // No hace nada dentro del bucle, solo espera
-  beep(); // Se llama a la función beep() cuando se presiona el botón
-}
-
-// Función para emitir un sonido
-void beep() {
-  tone(PINBUZZER, 2000, 100); // ReEmite un tono de 2000 Hz durante 100 ms
-  delay(200); // Espera 200 ms antes de continuar
-}
-
-void Peripherals_init() {
-  // Configuración de pines
-  pinMode(PINBOTON, INPUT); // El pin del botón se configura como entrada
-  pinMode(PINBUZZER, OUTPUT); // El pin del buzzer se configura como salida
-  pinMode(PINLED, OUTPUT); // El pin del LED se configura como salida
-
-
-}
