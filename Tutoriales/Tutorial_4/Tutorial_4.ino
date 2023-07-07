ddce92783bab9d0894473f7026a8f2947d975c4f
Andrés Bercovich <abercovich@lamborghino.com>
2023-07-07 17:25:56 -0400

    ok 8

--- Tutoriales/Tutorial_4/Tutorial_4.ino
+++ Tutoriales/Tutorial_4/Tutorial_4.ino
@@ -16,54 +16,25 @@ void setup() {
 
   Motores(0, 0);
 
-  Serial.println("hola, en esta tercera parte, nos aseguraremos que tenemos bien conectados los motores");
-  Serial.println("Instrucciones:   ");
-  Serial.println("");
+  Serial.println("hola, Cuarta parte del tutorial");
+  Serial.println("Ahora combinaremos lo aprendido en los tutoriales pasados.");
+  Serial.println("Para eso, primero calibraremos los sensores. Presiona el botón para calibrar...");
 
 
-  Serial.println("Cuando presiones el botón, los dos motores deberían ir hacia adelante.");
-  Serial.println("");
-
-  Serial.println("Si algún motor va hacia atrás, cambia la polaridad de conexión del motor, conectando el cable del motor al revés");
-  Serial.println("");
-
-  Serial.println("Ahora presiona el botón");
 
 
   delay(500);
   WaitBoton();
+  calibracion();
 
-  Motores(50, 50);
-
-  delay(1000);
-
-  Serial.println("");
-  Serial.println("");
-
-  Serial.println("Presiona el botón nuevamente para detener los motores");
-  Serial.println("");
-  Serial.println("");
-  Serial.println("");
-  Serial.println("");
-  WaitBoton();
-  Motores(0, 0);
-  delay(100);
-
-  Serial.println("Si los dos motores se mueven hacia adelante, entonces los motores ya están listos para utilizarse");
-  Serial.println("");
-
-  Serial.println("Para utilizar la función motores(x,y), solo debes cambiar la x y la y, por un valor entre -255 y 255.");
-  Serial.println("");
-
-  Serial.println("cuando x = 255, el motor izquierdo irá a la máxima potencia hacia adelante");
-  Serial.println("cuando x = -255, el motor izquierdo irá a la máxima potencia hacia atrás");
-  Serial.println("cuando x = 0, la potencia del motor izquierdo se establecerá en 0");
-  Serial.println("");
-
-
-  Serial.println("anlálogamente, pasará lo mismo con el motor derecho si se modifica la variable y ");
 
   WaitBoton();
+  Serial.println("ahora que tenemos el robot calibrado, intentaremos seguir una línea con el método de lego");
+  Serial.println("Es decir, si el robot está muy a la derecha, haremos que el motore de la derecha vaya hacia adelante...");
+  Serial.println("y si el robot está muy a la izquierda, haremos que el motor de la izquierda vaya hacia adelante");
+  Serial.println();
+  Serial.println();
+  Serial.println();
 
 
   delay(1000);
@@ -72,7 +43,20 @@ void setup() {
 
 void loop() {
   int p = GetPos();
-
   Serial.println(p);
+
+  if (p < 0) {
+
+    Motores(0, 100);
+  }
+
+
+  if (p > 0) {
+
+    Motores(100, 0);
+  }
+
+
+
   delay(1);
 }
